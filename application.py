bb343f8ef420b1afbbf5227891a0242583322b41
chunyen <jwt81728@yahoo.com.tw>
2021-01-17 16:26:15 +0800

    p4

--- application.py
+++ application.py
@@ -53,7 +53,8 @@ def callback():
 def handle_message(event):
     url_dict = {
       "TIBAME":"https://www.tibame.com/coursegoodjob/traffic_cli", 
-      "HELP":"https://developers.line.biz/zh-hant/docs/messaging-api/"}
+      "HELP":"https://developers.line.biz/zh-hant/docs/messaging-api/",
+      "LOCATION":"https://line.me/R/nv/location/"}
 # 將要發出去的文字變成TextSendMessage
     try:
         url = url_dict[event.message.text.upper()]
