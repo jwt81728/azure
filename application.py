59d1df2e3b147db5ffa5eaf0eae3cdfee38bd0cb
chunyen <jwt81728@yahoo.com.tw>
2021-01-17 15:27:25 +0800

    p

--- application.py
+++ application.py
@@ -24,6 +24,30 @@ from imgur_python import Imgur
 from PIL import Image, ImageDraw, ImageFont
 import time
 
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+
+LINE_SECRET = "7a33ea27fe76abfcb892fe6bab73d3e9"]
+LINE_TOKEN = "oyrxPFqR4Bq/DUtFNVB0HoZDBMFc+LlVuWtQgW4Rcf7UwMA/ZwZEiNDgQEC/5CjOfODkZJkqoj/QoE+MGcSombZckN07C1uPlLVERXCO+5MVwiDaouEtaXJxPol49+fG5iiyoJMToUbtsAQJxJZdhAdB04t89/1O/w1cDnyilFU="
+LINE_BOT = LineBotApi(LINE_TOKEN)
+HANDLER = WebhookHandler(LINE_SECRET)
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    # X-Line-Signature: 數位簽章
+    signature = request.headers["X-Line-Signature"]
+    print(signature)
+    body = request.get_data(as_text=True)
+    print(body)
+    try:
+        HANDLER.handle(body, signature)
+    except InvalidSignatureError:
+        print("Check the channel secret/access token.")
+        abort(400)
+    return "OK"
+
+
 app = Flask(__name__)
 
 
@@ -40,6 +64,8 @@ def hello():
             <center><h1>HI Bitch</h1></center>
         </body>
         """
+
+
 
 
     
