5bee35387a40783172dc7b558ddfda1989bba48e
chunyen <jwt81728@yahoo.com.tw>
2021-01-30 16:00:25 +0800

    boy

--- application.py
+++ application.py
@@ -43,7 +43,7 @@ CV_CLIENT = ComputerVisionClient(
 FACE_KEY = CONFIG["azure"]["face_key"]
 FACE_END = CONFIG["azure"]["face_end"]
 FACE_CLIENT = FaceClient(FACE_END, CognitiveServicesCredentials(FACE_KEY))
-PERSON_GROUP_ID = "tibame"
+PERSON_GROUP_ID = "boy"
 
 LINE_SECRET = CONFIG["line"]["line_secret"]
 LINE_TOKEN = CONFIG["line"]["line_token"]
